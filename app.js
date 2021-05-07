1f7365e00af73c6ebdda3313c0de891e6d64d702
Park Sang Jun <iteach12@gmail.com>
2021-05-07 13:05:14 +0900

    hoho

---
+++ app.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+range.style.color = "black";
+
+//canvas 초기화
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+let painting = false;
+
+function startPaintig(){
+    painting = true;
+}
+
+function stopPainting(){
+    painting = false;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        
+        ctx.beginPath();
+        
+    }else{
+        
+        ctx.lineTo(x,y);
+        ctx.stroke();
+        
+    }
+    
+}
+    
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    
+}
+
+function handleRangeChange(event){
+    ctx.lineWidth = event.target.value;
+    range.style.background = event.target.value;
+}
+
+
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+    );
+
+
