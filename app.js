a2c90564e7e4c50f2dc90e366c609412e768309d
Park Sang Jun <iteach12@gmail.com>
2021-05-07 13:43:28 +0900

    finish

--- app.js
+++ app.js
@@ -2,29 +2,49 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
-range.style.color = "black";
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+
+let painting = false;
+let filling = false;
 
 //canvas 초기화
-canvas.width = 700;
-canvas.height = 700;
-
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+
+
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 if(range){
     range.addEventListener("input", handleRangeChange);
 }
 
-let painting = false;
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
 
-function startPaintig(){
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
+
+
+function startPainting(){
     painting = true;
 }
 
@@ -43,21 +63,51 @@ function onMouseMove(event){
         
         ctx.lineTo(x,y);
         ctx.stroke();
-        
     }
-    
 }
-    
 
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
     
 }
 
 function handleRangeChange(event){
-    ctx.lineWidth = event.target.value;
-    range.style.background = event.target.value;
+    const size = event.target.value;
+    ctx.lineWidth = size;
+    
+}
+
+function handleModeClick(){
+
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+    }
+    
+}
+
+function handleCM(event){
+    event.preventDefault();
+
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]💚";
+    link.click();
 }
 
 
